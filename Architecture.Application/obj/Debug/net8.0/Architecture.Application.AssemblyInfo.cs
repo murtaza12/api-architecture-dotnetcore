641553f561e678988a93e80175dbeba6068134ec
Muhammad Murtaza <murtaza.mehbobi@gmail.com>
2024-07-26 01:40:46 +0500

    added gitignore

--- Architecture.Application/obj/Debug/net8.0/Architecture.Application.AssemblyInfo.cs
+++ Architecture.Application/obj/Debug/net8.0/Architecture.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Architecture.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b718488b314e8ffba618bbb9600a655b055137")]
 [assembly: System.Reflection.AssemblyProductAttribute("Architecture.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Architecture.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
